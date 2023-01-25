e93d8daf2c12f3c4c5d8376189e3dcec92196c08
Vladimir Schneider <schneider_v_s@mail.ru>
2023-01-26 00:47:25 +0300

    Sonar git Start 1

--- src/main/java/com/schneidervs/startmaven/StartMavenApplication.java
+++ src/main/java/com/schneidervs/startmaven/StartMavenApplication.java
@@ -10,7 +10,6 @@ public class StartMavenApplication {
     public static void main(final String[] args) {
         SpringApplication.run(StartMavenApplication.class, args);
         System.out.println("Hello world!");
-        System.out.println("Hello maven!");
         System.out.println("Hello Sonar!");
     }
 }
