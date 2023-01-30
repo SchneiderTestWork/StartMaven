e1d7fa0887d969f98a1b6f904568f65d6a8576c5
Vladimir Schneider <schneider_v_s@mail.ru>
2023-01-30 13:47:14 +0300

    Fix

--- src/main/java/com/schneidervs/startmaven/StartMavenApplication.java
+++ src/main/java/com/schneidervs/startmaven/StartMavenApplication.java
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartMavenApplication {
 
-
     public static void main(final String[] args) {
         SpringApplication.run(StartMavenApplication.class, args);
-        System.out.println("Hello world!");
-        System.out.println("Hello maven!");
         System.out.println("Hello Sonar!");
     }
 }
